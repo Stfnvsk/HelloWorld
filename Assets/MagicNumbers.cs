11e1ba850c0691273acbb23ab17cac7aff683df4
Stfnvsk <Stfnvsk@users.noreply.github.com>
2020-09-24 18:16:36 +0300

    Initial commit

---
+++ Assets/MagicNumbers.cs
@@ -0,0 +1,49 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicNumbers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int min = 1;
+    public int max = 1000;
+    int guess;
+    
+    void Start()
+    {
+        //min = 1;
+        //max = 1000;
+
+        print("Загадайте число от  " + min + "до" + max);
+        UpdateGuess();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            UpdateGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            UpdateGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            print("the end");
+            Start();
+        }
+    }
+    void UpdateGuess()
+    {
+        {
+            guess = (min + max) / 2;
+            print("Ваше число:" + guess + "?");
+        }
+    }
+}
+
---
+++ Assets/NumbersTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class NumbersTest : MonoBehaviour
+{
+    int number;
+    // Start is called before the first frame update
+    void Start()
+    {
+      number = 60;
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //number = number + 1;
+        //number += 1; // -=1 ;
+        number++; // number-- 
+        print(number);
+    }
+}
